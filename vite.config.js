11dbc4763e5a9e60a60462805f4bcc8e1265f423
Leonardo Bergonzoni <leonardbergonzon@MacBook-Air-di-Leonard.local>
2025-07-18 16:49:59 +0200

    Fix vite.config.js per Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Configurazione corretta per Netlify
 export default defineConfig({
-  base: '/Urbanda/',
+  base: './',
   plugins: [react()],
 })
